dae7c596e318e66701335ad447cd6ca3ab6af772
viktorsakman <viktorsakman@gmail.com>
2024-06-21 11:58:33 +0300

    Updating Website

--- src/components/BadgerBuds.jsx
+++ src/components/BadgerBuds.jsx
@@ -10,14 +10,12 @@ export default function BadgerBuds() {
 
     useEffect(() => {
         fetch('https://cs571.org/api/f23/hw5/buds', {
-            headers: {
-                "X-CS571-ID": "bid_608592443fea3ffac959c6020754a63b147a6c9eea3c58de71bf7d09318397bf"
-            }
+            
         })
-            .then(res => res.json())
-            .then(cats => {
-                setBuds(cats)
-            })
+        .then(res => res.json())
+        .then(cats => {
+            setBuds(cats)
+        })
     }, []);
 
     console.log(buds)
